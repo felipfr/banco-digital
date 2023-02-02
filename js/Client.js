ba48d8488ec308b9898fdd94b0a962e4f0bec96e
Felipe Rodrigues <99354371+felipfr@users.noreply.github.com>
2023-02-02 18:11:10 -0300

    Delete Client.js

--- js/Client.js
+++
@@ -1,9 +0,0 @@
-const Account = require("./Account");
-
-module.exports = class User {
-    constructor(fullName, email) {
-        this.fullName = fullName;
-        this.email = email;
-        this.account = new Account(this);
-    };
-};
